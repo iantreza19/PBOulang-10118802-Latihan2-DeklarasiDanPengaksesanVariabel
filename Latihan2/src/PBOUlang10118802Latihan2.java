02251d857f91abe55299a71d10e17bba739e7781
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-09-26 20:39:48 +0700

    Add files via upload

---
+++ Latihan2/src/PBOUlang10118802Latihan2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class PBOUlang10118802Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI= 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        System.out.println();
+        System.out.println("Isi variabel nilai = "+ nilaiInt);
+        System.out.println("Isi variabel PHI = "+ PHI);
+        System.out.println("Isi variabel logika = "+ nilaiLogika);
+        System.out.println("Isi variabel karakter = "+ nilaiKarakter);
+        
+    }
+    
+}
